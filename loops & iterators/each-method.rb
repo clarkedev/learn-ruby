7e4dae4a1d722f4c68f74e470c26ee271d7ed6ba
Will Clarke <willc@popcreative.co.uk>
2023-02-16 15:05:07 +0000

    loops: each method

---
+++ loops & iterators/each-method.rb
@@ -0,0 +1,27 @@
+=begin
+
+
+Each method in Ruby file
+
+Inside this file:
+* Each method
+* 
+
+
+=end
+
+# * Each method
+array = [1,2,3,4,5]
+
+array.each do |x|
+    x += 10 # Adds 10 to each number in the array as it loops
+    print "#{x}"
+end
+
+=begin
+
+
+End of file
+
+
+=end
