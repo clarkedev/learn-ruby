cdd899c07c31e385cb087b0ecee88795f7d0c449
Will Clarke <willc@popcreative.co.uk>
2023-02-16 14:51:11 +0000

    loops: for loop

---
+++ loops & iterators/for.rb
@@ -0,0 +1,28 @@
+=begin
+
+
+For loop in Ruby file
+
+Inside this file:
+* For loop
+* 
+
+
+=end
+
+# * For loop
+for num in 1...10 # Print up to the last number
+    puts num
+end
+
+for num2 in 1..10 # Print up to and including last number
+    puts num2
+end
+
+=begin
+
+
+End of file
+
+
+=end
