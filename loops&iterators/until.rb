c6c8160925ef1189453426c42f7954c609532e98
Will Clarke <willc@popcreative.co.uk>
2023-02-16 14:45:39 +0000

    loops: until loop

---
+++ loops&iterators/until.rb
@@ -0,0 +1,27 @@
+=begin
+
+
+Until loop in Ruby file
+
+Inside this file:
+* Until loop
+* 
+
+
+=end
+
+# * Until loop
+counter = 1 
+until counter > 10
+    puts counter 
+    counter = counter + 1
+end
+
+
+=begin
+
+
+End of file
+
+
+=end
