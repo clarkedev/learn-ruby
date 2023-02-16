05d617b833d73bf9aa1622ff11e9040055a407f5
Will Clarke <willc@popcreative.co.uk>
2023-02-16 14:54:28 +0000

    loops: loop method

---
+++ loops & iterators/loop-method.rb
@@ -0,0 +1,27 @@
+=begin
+
+
+Loop method in Ruby file
+
+Inside this file:
+* Loop method
+* 
+
+
+=end
+
+# * Loop method
+i = 0
+loop do
+    i += 1
+    print "#{i}"
+    break if i > 5
+end
+
+=begin
+
+
+End of file
+
+
+=end
