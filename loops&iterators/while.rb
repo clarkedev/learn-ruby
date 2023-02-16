d75ace1831c5786c6caadbfdc958329b60ae9653
Will Clarke <willc@popcreative.co.uk>
2023-02-16 14:43:40 +0000

    loops: while loop

---
+++ loops&iterators/while.rb
@@ -0,0 +1,26 @@
+=begin
+
+
+While loop in Ruby file
+
+Inside this file:
+* While loop
+* 
+
+
+=end
+
+# * While loop
+counter = 1 
+while counter < 11
+    puts counter
+    counter = counter + 1
+end
+
+=begin
+
+
+End of file
+
+
+=end
