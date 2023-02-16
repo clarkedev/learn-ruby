1f925f13ccf721a44de7bb603f9d0bc1516cb58a
Will Clarke <willc@popcreative.co.uk>
2023-02-16 15:10:00 +0000

    loops: time method

---
+++ loops & iterators/times-method.rb
@@ -0,0 +1,27 @@
+=begin
+
+
+Times method in Ruby file
+
+Inside this file:
+* Times method
+* 
+
+
+=end
+
+# * Times method
+10.times do 
+    print 'hi'
+end
+
+# * Short hand
+10.times {print 'hi'}
+
+=begin
+
+
+End of file
+
+
+=end
